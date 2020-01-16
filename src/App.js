bdd32457617c15cb1d3516421816d13abbe73f2b
majavrella <majavrella@gmail.com>
2020-01-16 13:06:48 +0530

    Normal and arrow functions testing

--- src/App.js
+++ src/App.js
@@ -1,26 +1,59 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+let sss = "ss";
+class App extends React.Component {
+  state = {
+    value: 'initialValue',
+  };
+  componentDidMount() {
+    this.add({ name: "vinay" });
+  }
+
+  add() {
+    console.log(arguments.nodeType);
+    // let sum = 0;
+    // for (let i = 0, j = arguments.length; i < j; i++) {
+    //   sum += arguments[i];
+    // }
+    // return sum;
+  }
+
+  handleEvent(textValue, boolValue) {
+    console.log('this', this); // `this` is an instance of class component
+    console.log('textValue', textValue);
+    console.log('boolValue', boolValue);
+    this.setState({ value: 'secondvalue' }); // setState should work without any error
+    function display() {
+      console.log(sss);
+    }
+
+    display();
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          {/* <img src={"https://img.icons8.com/material/4ac144/256/user-male.png"} className="App-logo" alt="logo" /> */}
+          <a
+            className="App-link"
+            href="https://majavrella.wixsite.com/thevinaysingh"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            State value {this.state.value}
+          </a>
+        </header>
+        <div>
+          <button
+            onClick={this.handleEvent.bind(this, "vinay", true)}>
+            Load More
+          </button>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
