1f4bd15be7c32b7e4a567c7486c1b9d659cb7787
majavrella <majavrella@gmail.com>
2020-01-27 20:34:13 +0530

    render svg as component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-import FunctionalComponent from './functionalComponent';
+import React from "react";
+import Logo from "./logo";
+import "./App.css";
+import FunctionalComponent from "./functionalComponent";
 
 class App extends React.Component {
   state = {
     value: 1
-  }
+  };
 
   componentDidMount() {
     setInterval(() => {
-      this.setState({value: 1});
+      this.setState({ value: 1 });
     }, 1000);
   }
 
   render() {
-    console.log("This console should be shown in every 1 sec as state update using set timeout");
+    console.log(
+      "This console should be shown in every 1 sec as state update using set timeout"
+    );
     return (
       <div className="default-container">
-        <p>{this.state.value}</p>
-        <FunctionalComponent value={this.state.value}/>
+        <p>State: {this.state.value}</p>
+        <p>Component:</p>
+        <FunctionalComponent value={this.state.value} />
+        <Logo className="App-logo" />
       </div>
     );
   }
