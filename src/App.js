57809f7e720e3c13c383b67703b736577967a296
majavrella <majavrella@gmail.com>
2020-01-27 17:06:56 +0530

    Redux implementation

--- src/App.js
+++ src/App.js
@@ -1,26 +1,31 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+import { connect } from "react-redux";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div className="default-container">
+
+        <p>{this.props.counterPropsForThisScreen}</p>
+        <button onClick={() => this.props.increment()}>Increment</button>
+        <button onClick={() => this.props.decrement()}>Decrement</button>
+        <button onClick={() => this.props.incrementByValue(10)}>Increment by 10</button>
+        <button onClick={() => this.props.decrementByValue(10)}>Decrement by 10</button>
+      </div>
+    );
+  }
 }
 
-export default App;
+const mapStateToProps = reduxState => ({
+  counterPropsForThisScreen: reduxState.counter
+});
+
+const mapDispatchToProps = dispatch => ({
+  increment: () => dispatch({ type: "INCREMENT" }),
+  decrement: () => dispatch({ type: "DECREMENT" }),
+  incrementByValue: valueToIncrement => dispatch({ type: "INCREMENT_BY_VALUE", value: valueToIncrement }),
+  decrementByValue: valueToDecrement => dispatch({ type: "DECREMENT_BY_VALUE", value: valueToDecrement})
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
--- src/index.js
+++ src/index.js
@@ -1,10 +1,20 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+import { createStore } from "redux";
+import "./index.css";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
+import counterReducer from './reducer/counterReducer';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const store = createStore(counterReducer);
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById("root")
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
